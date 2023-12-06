c84a8c9acab66ba7a0472edd473aa939c23cb03c
jrheard <jrheard@cs.stanford.edu>
2023-12-06 14:32:07 -0800

    WIP on day 1 part 2

--- day_1.ts
+++ day_1.ts
@@ -1,4 +1,4 @@
-function main(): number {
+function partOne(): number {
   const text: string = Deno.readTextFileSync("inputs/day_1.txt");
   const lines = text.split("\n");
   const digits = lines.map((line) =>
@@ -12,4 +12,43 @@ function main(): number {
   return calibrationValues.reduce((sum, value) => sum + value);
 }
 
-console.log(main());
+function partTwo(): number {
+  const text: string = Deno.readTextFileSync("inputs/day_1.txt");
+  const rawLines = text.split("\n");
+
+  const digitWords = {
+    "one": "1",
+    "two": "2",
+    "three": "3",
+    "four": "4",
+    "five": "5",
+    "six": "6",
+    "seven": "7",
+    "eight": "8",
+    "nine": "9",
+  };
+
+  const lines = rawLines.map((line) =>
+    line.replace(
+      /one|two|three|four|five|six|seven|eight|nine/g,
+      // TODO how to fix this error?
+      (match) => digitWords[match],
+    )
+  );
+
+  console.log(lines[1]);
+
+  const digits = lines.map((line) =>
+    line.split("").filter((char) => char >= "0" && char <= "9")
+  );
+
+  const calibrationValues = digits.map((line) =>
+    parseInt(line[0] + line.at(-1))
+  );
+
+  return calibrationValues.reduce((sum, value) => sum + value);
+}
+
+console.log(partOne());
+// TODO number too high - why?
+console.log(partTwo());
