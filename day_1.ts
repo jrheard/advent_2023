197d631dcd6eddcd05e73f21065e4cbbff343cf5
jrheard <jrheard@cs.stanford.edu>
2023-12-07 12:41:39 -0800

    day 1 part 2

--- day_1.ts
+++ day_1.ts
@@ -28,52 +28,17 @@ function partTwo(): number {
     "nine": "9",
   };
 
-  // Replace first occurrence
-  lines = lines.map((line) =>
-    line.replace(
-      /one|two|three|four|five|six|seven|eight|nine/,
-      (match) => digitWords[match as keyof typeof digitWords],
-    )
-  );
-
-  // Replace last occurrence
   lines = lines.map((line) => {
-    console.log(line);
-    let highestIndex = -1;
-    let farthestRightDigitWord = "";
-    let digit = "";
-
     for (const [k, v] of Object.entries(digitWords)) {
-      console.log(k, v);
-      const index = line.lastIndexOf(k);
-
-      if (index > highestIndex) {
-        console.log(index, highestIndex);
-        highestIndex = index;
-        farthestRightDigitWord = k;
-        digit = v;
-      }
+      line = line.replaceAll(k, k + v + k);
     }
 
-    if (highestIndex == -1) {
-      return line;
-    }
-
-    console.log(line.replaceAll(farthestRightDigitWord, digit));
-    return line.replaceAll(farthestRightDigitWord, digit);
+    return line;
   });
 
   const digits = lines.map((line) =>
     line.split("").filter((char) => char >= "0" && char <= "9")
   );
-
-  console.log(digits);
-
-  //console.log(digits[0]);
-  //console.log(digits[1]);
-  //console.log(digits[113]);
-  //console.log(digits.at(-2));
-  //console.log(digits.at(-1));
 
   const calibrationValues = digits.map((line) =>
     parseInt(line[0] + line.at(-1))
@@ -83,5 +48,4 @@ function partTwo(): number {
 }
 
 console.log(partOne());
-// TODO number too high - why?
 console.log(partTwo());
