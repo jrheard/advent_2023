988b783ba6b29cbbaba1e2c043cbca12dae77d2c
jrheard <jrheard@cs.stanford.edu>
2023-12-07 12:37:08 -0800

    WIP, doesn't work

--- day_1.ts
+++ day_1.ts
@@ -14,7 +14,7 @@ function partOne(): number {
 
 function partTwo(): number {
   const text: string = Deno.readTextFileSync("inputs/day_1.txt");
-  const rawLines = text.split("\n");
+  let lines = text.split("\n");
 
   const digitWords = {
     "one": "1",
@@ -28,21 +28,52 @@ function partTwo(): number {
     "nine": "9",
   };
 
-  const lines = rawLines.map((line) =>
+  // Replace first occurrence
+  lines = lines.map((line) =>
     line.replace(
-      /one|two|three|four|five|six|seven|eight|nine/g,
+      /one|two|three|four|five|six|seven|eight|nine/,
       (match) => digitWords[match as keyof typeof digitWords],
     )
   );
+
+  // Replace last occurrence
+  lines = lines.map((line) => {
+    console.log(line);
+    let highestIndex = -1;
+    let farthestRightDigitWord = "";
+    let digit = "";
+
+    for (const [k, v] of Object.entries(digitWords)) {
+      console.log(k, v);
+      const index = line.lastIndexOf(k);
+
+      if (index > highestIndex) {
+        console.log(index, highestIndex);
+        highestIndex = index;
+        farthestRightDigitWord = k;
+        digit = v;
+      }
+    }
+
+    if (highestIndex == -1) {
+      return line;
+    }
+
+    console.log(line.replaceAll(farthestRightDigitWord, digit));
+    return line.replaceAll(farthestRightDigitWord, digit);
+  });
 
   const digits = lines.map((line) =>
     line.split("").filter((char) => char >= "0" && char <= "9")
   );
 
-  console.log(digits[0]);
-  console.log(digits[1]);
-  console.log(digits.at(-2));
-  console.log(digits.at(-1));
+  console.log(digits);
+
+  //console.log(digits[0]);
+  //console.log(digits[1]);
+  //console.log(digits[113]);
+  //console.log(digits.at(-2));
+  //console.log(digits.at(-1));
 
   const calibrationValues = digits.map((line) =>
     parseInt(line[0] + line.at(-1))
