29167f3d4d62f5978924bcb6c512145697237a17
jrheard <jrheard@cs.stanford.edu>
2023-12-07 12:09:10 -0800

    WIP, still too high on day 1 part 2

--- day_1.ts
+++ day_1.ts
@@ -31,16 +31,18 @@ function partTwo(): number {
   const lines = rawLines.map((line) =>
     line.replace(
       /one|two|three|four|five|six|seven|eight|nine/g,
-      // TODO how to fix this error?
-      (match) => digitWords[match],
+      (match) => digitWords[match as keyof typeof digitWords],
     )
   );
-
-  console.log(lines[1]);
 
   const digits = lines.map((line) =>
     line.split("").filter((char) => char >= "0" && char <= "9")
   );
+
+  console.log(digits[0]);
+  console.log(digits[1]);
+  console.log(digits.at(-2));
+  console.log(digits.at(-1));
 
   const calibrationValues = digits.map((line) =>
     parseInt(line[0] + line.at(-1))
