37884dc9fc38bcdb547e59387667d23fb400daaa
jrheard <jrheard@cs.stanford.edu>
2023-12-17 11:12:43 -0800

    day 9 part 2

--- day_9.ts
+++ day_9.ts
@@ -37,9 +37,36 @@ function nextValueInSequence(sequence: readonly number[]): number {
   }
 }
 
+function previousValueInSequence(sequence: readonly number[]): number {
+  let currentSequence = sequence;
+  const sequenceStack = [];
+
+  while (true) {
+    if (isAllZeroes(currentSequence)) {
+      let currentStartValue = 0;
+
+      while (sequenceStack.length > 0) {
+        const previousSequenceStartValue = sequenceStack.pop()![0];
+        currentStartValue = previousSequenceStartValue - currentStartValue;
+      }
+
+      return currentStartValue;
+    } else {
+      sequenceStack.push(currentSequence);
+      currentSequence = differences(currentSequence);
+    }
+  }
+}
+
 function partOne(): number {
   const input = parseInput();
   return input.map(nextValueInSequence).reduce((acc, val) => acc + val);
 }
 
+function partTwo(): number {
+  const input = parseInput();
+  return input.map(previousValueInSequence).reduce((acc, val) => acc + val);
+}
+
 console.log(partOne());
+console.log(partTwo());
